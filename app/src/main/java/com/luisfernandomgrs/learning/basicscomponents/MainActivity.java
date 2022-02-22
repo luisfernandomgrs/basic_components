461871d5fffa2ac2a6e3e0851e94f71251b8ccac
Luis Fernando Machado <luisfernandomgrs@gmail.com>
2022-02-21 23:10:18 -0300

    first commit, project for dammies

---
+++ app/src/main/java/com/luisfernandomgrs/learning/basicscomponents/MainActivity.java
@@ -0,0 +1,15 @@
+package com.luisfernandomgrs.learning.basicscomponents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle(getString(R.string.app_title));
+    }
+}
